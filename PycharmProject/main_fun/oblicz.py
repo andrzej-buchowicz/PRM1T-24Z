d395f594d84fa23ea33940e0b13e4f509d6634e3
Andrzej Buchowicz <andrzej.buchowicz@gmail.com>
2024-11-07 20:24:13 +0100

    bez if __name__

---
+++ PycharmProject/main_fun/oblicz.py
@@ -0,0 +1,5 @@
+from silnia import silnia_iter
+
+
+n = int(input("Podaj liczbę: "))
+print(n, silnia_iter(n))
---
+++ PycharmProject/main_fun/silnia.py
@@ -0,0 +1,18 @@
+def silnia_iter(n: int) -> int:
+    val = 1
+    for i in range(2, n + 1):
+        val *= i
+
+    return val
+
+
+def silnia_rekur(n: int) -> int:
+    if n <= 1:
+        return 1
+    else:
+        return n * silnia_rekur(n - 1)
+
+
+for n in range(10):
+    print(n, silnia_iter(n), silnia_rekur(n))
+
