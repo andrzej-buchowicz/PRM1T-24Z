df7d14457a811f8c736b285cf41b7d88b1753aa5
Andrzej Buchowicz <andrzej.buchowicz@gmail.com>
2024-12-04 22:19:07 +0100

    unit 3

--- PycharmProject/unit/sinus.py
+++ PycharmProject/unit/sinus.py
@@ -1,2 +1,13 @@
-def sinus(x: float) -> float:
-    return 0.
+import math
+
+
+def sinus(x: float, N: int=10) -> float:
+    assert isinstance(x, (int, float))
+
+    val = 0.
+    n = 0
+    for n in range(N):
+        comp = math.pow(-1., n) * math.pow(x, 2 * n + 1) / math.factorial(2 * n + 1)
+        val += comp
+
+    return val
