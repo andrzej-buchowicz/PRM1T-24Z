8b043f6b3c256695276754f581ae4bba12d17b70
Andrzej Buchowicz <andrzej.buchowicz@gmail.com>
2024-12-04 22:21:41 +0100

    unit 5

--- PycharmProject/unit/test_sinus.py
+++ PycharmProject/unit/test_sinus.py
@@ -7,9 +7,9 @@ from sinus import sinus
 class SinusTestCase(unittest.TestCase):
     def test_poprawny_wynik(self):
         wynik = sinus(0)
-        self.assertEqual(wynik, 0.)
-        self.assertEqual(sinus(math.pi / 4), math.sqrt(2.) / 2)
-        self.assertEqual(sinus(math.pi / 2), 1.0)
+        self.assertAlmostEqual(sinus(0.), 0.)
+        self.assertAlmostEqual(sinus(math.pi/4), math.sqrt(2.)/2)
+        self.assertAlmostEqual(sinus(math.pi/2), 1.0)
 
 
 if __name__ == '__main__':
